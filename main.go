afa6ee7f4e58d37dd5af5ab23a3e9f2ed403c298
hendisantika <hendisantika@yahoo.co.id>
2024-08-14 20:32:46 +0700

    Add handleRequest func🫸🌀✏️📗🐧🐳

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 )
 
+func handleRequest(writer http.ResponseWriter, request *http.Request) {}
+
 func main() {
 	http.HandleFunc("/generate", handleRequest)
 	http.ListenAndServe(":8080", nil)
